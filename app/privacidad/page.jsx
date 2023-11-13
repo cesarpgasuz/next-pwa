5d518adbfdddc981ce891cd96d720ae69731984c
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2023-11-12 20:21:03 -0600

    aviso de privacidad

---
+++ app/privacidad/page.jsx
@@ -0,0 +1,40 @@
+function Privacidad() {
+  return (
+    <div>
+        <h1>Política de Privacidad de Next-pwa</h1>
+
+<p>
+Información Recopilada
+1. Datos de Uso
+Next-pwa recopila información anónima sobre el uso de la aplicación con el fin de mejorar su funcionalidad y rendimiento. Esto incluye, pero no se limita a, datos sobre las páginas visitadas, el tiempo de uso y las interacciones con la interfaz de usuario.
+
+2. Dirección IP
+La aplicación Next-pwa puede registrar y almacenar direcciones IP con fines de seguridad y análisis. Estos datos se utilizan de manera interna y no se comparten con terceros.
+
+3. Geolocalización
+Next-pwa puede utilizar la geolocalización para mejorar la experiencia del usuario. Sin embargo, esta información no se compartirá con terceros y se utilizará exclusivamente dentro de la aplicación para brindar servicios relacionados con la ubicación.
+
+Uso de la Información
+La información recopilada se utiliza para:
+
+Mejorar y personalizar la experiencia del usuario.
+Analizar el rendimiento y la usabilidad de la aplicación.
+Garantizar la seguridad de la aplicación y prevenir actividades maliciosas.
+Compartir Información
+Next-pwa no comparte información personal con terceros. La dirección IP y la geolocalización se utilizan internamente y no se divulgan fuera de los sistemas de la aplicación.
+
+Cookies
+Next-pwa puede utilizar cookies para mejorar la funcionalidad y la experiencia del usuario. Estas cookies no contienen información personal identificable y se utilizan únicamente con fines técnicos y analíticos.
+
+Seguridad
+Se implementan medidas de seguridad para proteger la información recopilada por Next-pwa y garantizar su confidencialidad.
+
+Cambios en la Política de Privacidad
+Next-pwa se reserva el derecho de actualizar esta Política de Privacidad en cualquier momento. Los cambios se publicarán en esta página.
+
+Contacto
+Si tienes preguntas o inquietudes sobre esta Política de Privacidad, puedes contactarnos.    
+    </p></div>
+  )
+}
+export default Privacidad
